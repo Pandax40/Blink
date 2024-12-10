2875706aa86320bfe153747ac50585b8bee40d32
Pandax40 <pandaxsantin@gmail.com>
2024-12-10 18:24:28 +0100

    Firebase ktx import

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.stream.webrtc.android)
     implementation(libs.stream.webrtc.android.compose)
     implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
