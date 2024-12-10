7fc3a51403b8afd93ba7ed6849706692749cbe8f
Pandax40 <pandaxsantin@gmail.com>
2024-12-10 01:05:45 +0100

    WebRTC and WebRTC for Compose SDK added (by GetStream)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.stream.webrtc.android)
+    implementation(libs.stream.webrtc.android.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
