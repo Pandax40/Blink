4900f9b7eadd689bb7ecb0a117d5644ac3d35594
Pandax40 <pandaxsantin@gmail.com>
2024-12-27 18:28:44 +0100

    Removed unused imports

--- app/src/main/java/com/pandina/blink/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/pandina/blink/ui/screens/HomeScreen.kt
@@ -4,12 +4,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.Button
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -17,7 +15,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pandina.blink.R
-import com.pandina.blink.data.repository.UserRepository
 
 @Composable
 fun HomeScreen(onBlinkClick: () -> Unit) {
