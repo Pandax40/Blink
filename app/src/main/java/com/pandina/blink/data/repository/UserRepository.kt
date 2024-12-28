2b0d676133eeb61e23deb7cce95e4f54d90ac1ce
Pandax40 <pandaxsantin@gmail.com>
2024-12-28 18:40:49 +0100

    Delete unused UserRepository.kt

--- app/src/main/java/com/pandina/blink/data/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.pandina.blink.data.repository
-
-class UserRepository {
-    fun getConnectedUsers(): Int {
-        // Lógica real o simulada para obtener usuarios conectados
-        return (1..1000).random() // Simulación con número aleatorio
-    }
-}
